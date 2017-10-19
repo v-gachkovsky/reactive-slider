b640cc888d0f48fde46ab66173f73d839d0c21c7
v-gachkovsky <v.gachkovsky@gmail.com>
2017-10-19 22:52:00 +0300

    Init Commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+class ReactiveSlider extends React.Component {
+  render() {
+    return (
+      <div>Reactive-Slider</div>
+    );
+  }
+}
+export default ReactiveSlider;
