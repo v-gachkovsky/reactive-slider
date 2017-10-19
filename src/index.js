31cf862fc427a64bdfd55c9c6de3c5973a9797c4
v-gachkovsky <v.gachkovsky@gmail.com>
2017-10-20 00:19:09 +0300

    build list of slides

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 class ReactiveSlider extends React.Component {
   render() {
+    if (!this.props.slides) { return <div><p>Slides not present</p></div> }
+    const slides = this.props.slides.map((slide, index) => {
+      return <div className="slide" key={index}>
+        <img src={slide}/>
+      </div>
+    });
+
     return (
-      <div>Reactive-Slider</div>
+      <div>
+        <h4>Reactive-Slider</h4>
+        <div id="reactive-slider">
+          {slides}
+        </div>
+      </div>
     );
   }
 }
